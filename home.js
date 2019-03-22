beeff6bb33d047c853a3a0057bc12611cb1132c9
S.Tar <santana.tarasewich@gmail.com>
2019-03-22 12:58:28 -0700

    image upload

--- home.js
+++ home.js
@@ -157,7 +157,33 @@
     })(jQuery);
 
     $("#postInput").charCounter(320,{container:'#counter'});
-    console.log('hello');
+    
+ 
+      $('#uploadForm').on('submit',function(){
+
+        var fd = new FormData();
+        var files = $('#file')[0].files[0];
+        fd.append('file',files);
+
+        // AJAX request
+        $.ajax({
+          url: 'uploads.php',
+          type: 'post',
+          data: fd,
+          contentType: false,
+          processData: false,
+          success: function(response){
+            if(response != 0){
+              // Show image preview
+              $('#preview').append("<img src='"+response+"' width='100' height='100' style='display: inline-block;'>");
+            }else{
+              alert('file not uploaded');
+            }
+          }
+        });
+        return false;
+      });
+
       
 });
 
